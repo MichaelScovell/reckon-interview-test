cf57715a718c1afc32c5a6b48035e2376934e9ca
MichaelScovell <michael.scovell@cevo.com.au>
2023-06-29 23:01:55 +1000

    Implemented 2 second increment and cleaned up comments and styling

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
 // Defining imports for the counting application
+import { React, useState, useEffect, useRef } from 'react';
 import './App.css';
-import { React, useState, useEffect } from 'react';
 
-// Defining a function to house the logic for the application
 function App() {
   // Defining variables to store state for the counter
   const [counter, setCounter] = useState(0);
+  // Defined a useRef for referencing the counter within the setInterval function
+  const counterRef = useRef(counter);
+  counterRef.current = counter;
 
-  // Define a function to handle incrementing
+  // Defining a useEffect hook to invoke setInterval at 2 seconds and update the counter
+  useEffect(() => {
+    const timer = setInterval(() => {
+      // Increment the counter using the reference
+      setCounter(counterRef.current + 1);
+      return () => clearInterval(timer);
+    }, 2000);
+  }, []);
+
+  // Define a function to handle manual incrementing
   function handleIncrement() {
     // Increase the counter
-    setCounter(counter + 1);
+    setCounter(counterRef.current + 1);
   }
-
 
   // UI for the counting application 
   return (
     <div className="App">
-      <h1>Counter: {counter}</h1>
+      <h1>Counter: {counterRef.current}</h1>
       {/* Increment Button */}
       <button onClick={handleIncrement}>Increment</button>
     </div>
